01b5225d7ca57f8f62795e35c4c6f409a657011f
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-10-30 21:05:14 +0100

    implements #4

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -1,4 +1,11 @@
 class AuthorController < ApplicationController
   def new
   end
+
+  def create
+    puts params
+    @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
+    @author.save
+    redirect_to root_path, notice: 'Success!'
+  end
 end
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -8,10 +8,18 @@ describe "New author page", type: :feature do
 
   it "should have text inputs for an author's first name, last name, and homepage" do
     visit new_author_path
-
-    # these are the standard names given to inputs by the form builder
     expect(page).to have_field('author[first_name]')
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
+  it "should save the new author in the database" do
+    visit new_author_path
+    page.fill_in 'author[first_name]', with: 'Alan'
+    page.fill_in 'author[last_name]', with: 'Turing'
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+    author = Author.where(:first_name => 'Alan', :last_name => 'Turing').first
+    expect(author.name).to(eq("Alan Turing"))
+ end
 end

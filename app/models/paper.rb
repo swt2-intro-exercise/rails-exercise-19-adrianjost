bb53bf7857592b5dded9c368626cbeee7d9975ac
adrianjost <22987140+adrianjost@users.noreply.github.com>
2019-11-01 15:05:14 +0100

    implement #19

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,5 +1,7 @@
 class Paper < ApplicationRecord
   has_and_belongs_to_many :authors
+  #query by year
+  scope :in_year, ->(year) { where("year = ?", year) }
   #allow update
   attr_accessor :author_ids
   validates :title, presence: true
--- spec/features/paper/show_papers_spec.rb
+++ spec/features/paper/show_papers_spec.rb
@@ -2,30 +2,33 @@ require 'rails_helper'
 
 describe "show papers page", type: :feature do
   before do
-    @paper = FactoryBot.create :paper
+    papers = FactoryBot.create_list(:paper, 2)
+    @paper1 = papers[0]
+    @paper2 = papers[1]
+    @paper2.update(year: @paper2.year - 1, title: "Some other book the author wrote")
   end
 
   it "should render withour error" do
-    visit paper_path(@paper)
+    visit paper_path(@paper1)
   end
 
   it "should display title" do
-    visit paper_path(@paper)
-    expect(page).to have_text(@paper.title)
+    visit paper_path(@paper1)
+    expect(page).to have_text(@paper1.title)
   end
 
   it "doesn't shows the author if there are none" do
-    @paperWithoutAuthor = FactoryBot.create :paper_without_author
-    visit paper_path(@paperWithoutAuthor)
-    expect(@paperWithoutAuthor.authors.count).to be 0
+    @paper1WithoutAuthor = FactoryBot.create :paper_without_author
+    visit paper_path(@paper1WithoutAuthor)
+    expect(@paper1WithoutAuthor.authors.count).to be 0
     expect(page).not_to have_text("Author")
   end
 
   it "shows the author if there is one" do
-    visit paper_path(@paper)
-    expect(@paper.authors.count).to be 1
+    visit paper_path(@paper1)
+    expect(@paper1.authors.count).to be 1
     expect(page).to have_text("Author:")
-    expect(page).to have_text(@paper.authors[0].name)
+    expect(page).to have_text(@paper1.authors[0].name)
   end
 
   it "shows the authors if there are some" do
@@ -36,5 +39,15 @@ describe "show papers page", type: :feature do
     @authorsPaper.authors.each do |author|
       expect(page).to have_text(author.name)
     end
+  end
+
+  it "should filter papers with other years out" do
+    visit (papers_path + "?year=" + String(@paper2.year))
+    expect(page).to_not have_text(@paper1.title)
+  end
+
+  it "should show papers with the correct year" do
+    visit (papers_path + "?year=" + String(@paper2.year))
+    expect(page).to have_text(@paper2.title)
   end
 end
